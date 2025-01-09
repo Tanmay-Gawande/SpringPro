6a677b93f7a36b4c3ffc4ee028581f0624ffcce3
10743087 <10743087@5CG2281ZZ2.bwrouter>
2025-01-10 00:28:50 +0530

    First Commit

---
+++ SpringPro/src/test/java/com/javatoc/SpringProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
